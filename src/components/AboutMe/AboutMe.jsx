0fc4567e7bb34de877547c7334799dc31f1d4d0d
Aslay Clevisson <aslayclei@gmail.com>
2021-07-10 02:08:14 -0300

    Feature(main): Organization of css and html structure - New font added

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -8,26 +8,42 @@ function aboutInfo() {
 
   return (
     <section className="container">
-        <div>
-            <h1>Hey, I'm Aslay</h1>
-            <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque saepe sint numquam omnis dolorum magnam incidunt magni quo commodi placeat quas atque illum beatae enim quibusdam, ullam dolorem deleniti cumque?
-            </p>
+        <div className="AboutMe-description main-description">
+            <div className="WAMI">
+                <h1>Hey, I'm Aslay</h1>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                </p>
+            </div>
+
+            <div className="Grid-description">
+                <div className="AboutMe">
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet, delectus non totam at qui ratione labore eaque illum cumque atque nihil ea vel officiis placeat voluptatum, ipsum laborum vero nobis.
+                    </p>
+                </div>
+
+                <div className="AboutEx">
+                    <p>
+                        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Perspiciatis molestias distinctio consequuntur inventore, harum numquam praesentium incidunt fugiat aliquid, nam, id consectetur velit rerum obcaecati eum atque. Inventore, ipsam sed?
+                    </p>
+                </div>
+            </div>
         </div>
-        <div class="background-animate" >
-                <ul class="transition-animate">
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                </ul>
+        <div className="background-animate" >
+            <ul className="transition-animate">
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
     </div >
     </section>
   );
